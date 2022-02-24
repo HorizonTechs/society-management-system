69301472c5ce0bcd49aed2619b62a22092d0417c
HDWares <deepeshsom@gmail.com>
2022-02-24 12:15:42 +0530

    changed app import order

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from app_modules import create_app
 
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(debug=False)
-
 app = create_app()
-app.run(debug=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
