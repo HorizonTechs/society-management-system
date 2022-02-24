45a6081cc4f66d578b3b950d90a0080e2a49fd76
HDWares <deepeshsom@gmail.com>
2022-02-24 23:24:15 +0530

    front end separated and env variables

--- app_modules/__init__.py
+++ app_modules/__init__.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from .models import db
 from .views import view as view_blueprint
 
 def create_app(config_file='settings.py'):
-    app = Flask(__name__)
+    frondend_folder = '../frontend'
+
+    app = Flask(__name__, template_folder=frondend_folder, static_folder=frondend_folder + '/static')
     CORS(app)
     app.config.from_pyfile(config_file)
     db.init_app(app)
