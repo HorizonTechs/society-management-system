ff9fc6dc48817a6c5ebf48f211ba7c8f4fede814
HDWares <deepeshsom@gmail.com>
2022-02-24 23:34:47 +0530

    replaced postgres to prostgresql as per update

--- app_modules/settings.py
+++ app_modules/settings.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL').replace("postgres://", "postgresql://")
 SECRET_KEY = getenv('SECRET_KEY')
 PASSWORD_RESET_KEY = getenv('PASSWORD_RESET_KEY')
 CAP_KEY = getenv('CAP_KEY')
