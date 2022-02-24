c7de5e3734abfef1a45cd6a406e68008135ae997
HDWares <deepeshsom@gmail.com>
2022-02-24 12:04:26 +0530

    app in app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from app_modules import create_app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=False)
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(debug=False)
+
+app = create_app()
+app.run(debug=False)
